b662cb2925b487912a716cee06dd00726f674c1b
cardinaldeacre <cardinaldeacre@gmail.com>
2026-08-26 08:51:35 +0700

    change function name

--- lib/services/portofolio.ts
+++ lib/services/portofolio.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@/lib/supabase/server';
 
-export async function getPortfolio() {
+export async function getPublicPortfolioData() {
     const supabase = await createClient();
 
     const [
